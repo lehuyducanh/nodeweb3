8bf8e1b3dcb2046b4f7c4cc938ff73fc57f98fc4
lehuyducanh <lehuyducanh@gmail.com>
2024-03-10 01:02:08 +0700

    Update:

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-module.exports = function multiply(a, b) {
-  return a * b;
-};
+class NodeNumpy {
+  static add(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Array lengths must be equal");
+    }
+    return arr1.map((val, index) => val + arr2[index]);
+  }
+
+  static subtract(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Array lengths must be equal");
+    }
+    return arr1.map((val, index) => val - arr2[index]);
+  }
+
+  static multiply(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Array lengths must be equal");
+    }
+    return arr1.map((val, index) => val * arr2[index]);
+  }
+
+  static divide(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Array lengths must be equal");
+    }
+    return arr1.map((val, index) => val / arr2[index]);
+  }
+
+  static dot(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Array lengths must be equal");
+    }
+    return arr1.reduce((sum, val, index) => sum + val * arr2[index], 0);
+  }
+
+  static sum(arr) {
+    return arr.reduce((sum, val) => sum + val, 0);
+  }
+
+  static mean(arr) {
+    return NodeNumpy.sum(arr) / arr.length;
+  }
+
+  static max(arr) {
+    return Math.max(...arr);
+  }
+
+  static min(arr) {
+    return Math.min(...arr);
+  }
+}
